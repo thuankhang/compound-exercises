19e0b07e4648617ad2d0730629ba58514e901f0f
thuankhang <matthuanphong@gmail.com>
2021-12-21 15:13:17 +0700

    feat Homework: Compound Exercises

---
+++ app/src/main/java/com/example/appemail/BarFragment.kt
@@ -0,0 +1,27 @@
+package com.example.appemail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.appemail.databinding.ActivityMainBinding
+
+class BarFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+        return inflater.inflate(R.layout.fragment_bar, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/appemail/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.appemail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appemail.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
